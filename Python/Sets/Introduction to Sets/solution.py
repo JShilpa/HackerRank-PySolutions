bd712e66bbfb448c6176776b8666ba96339441f1
Shilpa Jain <shilpa.jain2014@outlook.com>
2015-05-04 20:00:00 -0600

    adding sets solution

--- Python/Sets/Introduction to Sets/solution.py
+++ Python/Sets/Introduction to Sets/solution.py
@@ -6,9 +6,9 @@ def average(array):
     distinct_height = set(array)
     return sum(distinct_height) / len(distinct_height)
 
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
-
