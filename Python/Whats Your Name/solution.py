2da87ded825248ebcaa204206ea339ccde08c1af
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-04-22 22:48:36 +0530

    adding print format solution

---
+++ Python/Whats Your Name/solution.py
@@ -0,0 +1,7 @@
+# Strings
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+def print_full_name(a, b):
+    print('Hello {a} {b}! You just delved into python.'.format(a=a, b=b))
+
+print_full_name(input(), input())
