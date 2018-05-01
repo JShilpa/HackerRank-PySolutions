10393980b90d0f9a244c3c4a2d7291b7c38385e7
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-05-01 22:50:10 +0530

    adding sets solutions

--- Python/Sets/Set .symmetric_difference() Operation/solution.py
+++ Python/Sets/Set .symmetric_difference() Operation/solution.py
@@ -2,9 +2,7 @@
 # https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem
 
 
-
-
-m, english = input(), set(map(int,input.split()))
-n, french = input(), set(map(int,input.split()))
+m, english = input(), set(map(int, input().split()))
+n, french = input(), set(map(int, input().split()))
 
 print(len(english.symmetric_difference(french)))
