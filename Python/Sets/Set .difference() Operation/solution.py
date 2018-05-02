62e9268baa655d8222a2fd0673ae70c6790e87b4
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-05-02 21:36:29 +0530

    moved set difference operator program

---
+++ Python/Sets/Set .difference() Operation/solution.py
@@ -0,0 +1,8 @@
+# Sets Questions
+# https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+
+
+m, english = input(), set(map(int, input.split()))
+n, french = input(), set(map(int, input.split()))
+
+print(len(english.difference(french)))
