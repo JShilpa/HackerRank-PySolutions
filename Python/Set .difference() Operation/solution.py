3af6538aa9dc6c3bd874e71f1a3a0a85c406c9cf
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-05-05 21:35:31 +0530

    delete solution.py

--- Python/Set .difference() Operation/solution.py
+++
@@ -1,8 +0,0 @@
-# Sets Questions
-# https://www.hackerrank.com/challenges/py-set-difference-operation/problem
-
-
-m, english = input(), set(map(int, input.split()))
-n, french = input(), set(map(int, input.split()))
-
-print(len(english.difference(french)))
