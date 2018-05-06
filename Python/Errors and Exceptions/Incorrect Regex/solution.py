65e5cf114010511ed54059eb478a95755c7b5efa
Shilpa Jain <shilpa.jain2014@outlook.com>
2018-05-06 23:22:25 +0530

    adding incorrect regex exception solution

---
+++ Python/Errors and Exceptions/Incorrect Regex/solution.py
@@ -0,0 +1,12 @@
+# Exceptions Problem
+# https://www.hackerrank.com/challenges/incorrect-regex/problem
+
+
+import re
+
+for _ in range(int(input())):
+    try:
+        re.compile(input())
+        print('True')
+    except re.error:
+        print("False")
